4bdcc3f50dde72eef9b8c08df4df93c4c432393f
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2022-11-09 18:12:10 +0300

    assets added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,14 +1,4 @@
-const getSource = (path) => {
-  const module = require(`./${path}`);
-  const src = module.default;
-  return src;
-};
-const createImage = (src) => {
-  const img = new Image();
-  img.src = src;
-  return img;
-};
-
 export default {
-  image: createImage(getSource(`image.jpg`)),
+  images: `./assets/assets.json`,
+  bg: `./assets/bg.jpg`,
 };
