be06d60c2ce6fa30f99080a9a5ca533497101dd5
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2022-11-09 18:14:42 +0300

    added links

--- src/scripts/model/project.js
+++ src/scripts/model/project.js
@@ -1,4 +1,8 @@
 export default {
   orientationRatio: 1,
   platform: `default`,
+  links: {
+    ios: `https://apps.apple.com/ru/app/homescapes/id1195621598`,
+    gp: `https://play.google.com/store/apps/details?id=com.playrix.homescapes&hl=en_US&gl=US`,
+  },
 };
