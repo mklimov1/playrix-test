a5e3c1fde5a91876d14cda040f38b23815d70a44
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2022-11-10 15:55:21 +0300

    updated project data

--- src/scripts/model/project.js
+++ src/scripts/model/project.js
@@ -1,7 +1,6 @@
 export default {
-  orientationRatio: 1,
-  platform: `default`,
   links: {
+    ms: `https://www.microsoft.com/en-us/p/homescapes/9nq9klznv9sh?activetab=pivot:overviewtab`,
     ios: `https://apps.apple.com/ru/app/homescapes/id1195621598`,
     gp: `https://play.google.com/store/apps/details?id=com.playrix.homescapes&hl=en_US&gl=US`,
   },
