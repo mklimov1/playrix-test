9f1f8c9e304c5243b5fc1ff813b5d669bd0cb73f
Maxim Klimov <maximklimov@MacBook-Air-Maxim.local>
2022-12-05 14:44:12 +0300

    removed unnecessary code

--- wp_config/platforms/default.js
+++
@@ -1,3 +0,0 @@
-export default {
-  isSingle: false,
-};
--- wp_config/project.config.js
+++ wp_config/project.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   engine2d: true,
-  engine3d: true,
-  sounds: true,
-  animationEngine: true,
-  spine: true,
-  title: ``,
-  projectTitle: ``,
-
-  platform: null,
 
   links: {
     as: ``,
